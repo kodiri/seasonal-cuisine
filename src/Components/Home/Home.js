a35f432c3ce895d429a0630a8b8d60b4ce56be6b
Alexandra Pascal <alexandra.pascal88@yahoo.com>
2018-12-11 20:21:36 +0000

    create placeholder for the dropdown

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,31 @@
 import React from "react";
 import Hero from "./Hero.js";
+import "./Home.css";
 
 export default function Routes() {
   return (
     <div>
       <Hero />
+      <div className="content">this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
+      <div>this is content</div>
       <div>this is content</div>
     </div>
   );
